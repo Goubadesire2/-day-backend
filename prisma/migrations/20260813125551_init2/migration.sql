f18f75bcba63de9c769ef31e55ae30e7f05d7c89
Goubadesire2 <goubadesire0@gmail.com>
2026-08-13 13:15:38 +0000

    fix: correct mysql table case in migration

--- prisma/migrations/20260813125551_init2/migration.sql
+++ prisma/migrations/20260813125551_init2/migration.sql
@@ -2,4 +2,7 @@
 DROP INDEX `Task_user_id_fkey` ON `task`;
 
 -- AddForeignKey
-ALTER TABLE `Task` ADD CONSTRAINT `Task_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `task`
+ADD CONSTRAINT `Task_user_id_fkey`
+FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+ON DELETE RESTRICT ON UPDATE CASCADE;
